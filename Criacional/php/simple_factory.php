d9b6a35a95797c685021afa2da2f74c295cce02b
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-17 12:02:46 -0400

    rename pattern

---
+++ Criacional/php/simple_factory.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * ----------------------------------------------*
+ *                 SIMPLE FACTORY                |
+ * ----------------------------------------------*
+ */
+
+declare(strict_types=1);
+
+interface CarroInterface {
+    public function dirigir();
+}
+
+class Carro implements CarroInterface {
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function dirigir() {
+        return "Vruummmm...";
+    }
+}
+
+final class Amarok extends Carro {
+    public function __construct(string $name) {
+        parent::__construct($name);
+    }
+}
+
+final class HB20 extends Carro {
+    public function __construct(string $name) {
+        parent::__construct($name);
+    }
+}
+
+final class Prisma extends Carro {
+    public function __construct(string $name) {
+        parent::__construct($name);
+    }
+}
+
+abstract class CarroFactory {
+    public static function create(string $name): CarroInterface {
+        return new $name($name);
+    }
+}
+
+$carro1 = CarroFactory::create('Amarok');
+$carro2 = CarroFactory::create('HB20');
+$carro3 = CarroFactory::create('Prisma');
+
+var_dump($carro1, $carro2, $carro3);
