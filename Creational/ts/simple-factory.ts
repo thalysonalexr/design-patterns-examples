659e48157a09e516825c5fbd9e81d8c69379f557
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-24 22:22:36 -0400

    add simple-factory pattern and rename factory-method

--- Creational/ts/simple-factory.ts
+++ Creational/ts/simple-factory.ts
@@ -1,67 +1,18 @@
 export namespace simpleFactoryExample {
-  interface CarInterface {
-    drive(): string
-    getName(): string
+  
+  export class User {
+    constructor(private _username: string, private _age: number) {}
+    public get username(): string { return this._username }
+    public get age(): number { return this._age }
   }
-  
-  class Car implements CarInterface {
-    constructor(private _name: string) {}
-  
-    public drive() {
-      return 'Vrummmmm...'
-    }
-  
-    public getName(): string {
-      return `Car: ${this.name}`
-    }
-  
-    public get name(): string {
-      return this._name;
+
+  abstract class UserFactory {
+    public static create(username: string, age: number): User {
+      return new User(username, age)
     }
   }
-  
-  class Amarok extends Car {
-    constructor(name: string) { super(name) }
-  }
-  
-  class HB20 extends Car {
-    constructor(name: string) { super(name) }
-  }
-  
-  class Prisma extends Car {
-    constructor(name: string) { super(name) }
-  }
-  
-  type CarType = 'Amarok' | 'HB20' | 'Prisma' | 'Car';
-  
-  abstract class CarFactory {
-    public static create(car: CarType | undefined = undefined): CarInterface {
-      let instance: CarInterface;
-      switch (car) {
-        case 'Amarok':
-          instance = new Amarok('Amarok');
-          break;
-        case 'HB20':
-          instance = new HB20('HB20');
-          break;
-        case 'Prisma':
-          instance = new Prisma('Prisma');
-          break;
-        default:
-          instance = new Car('Car');
-          break;
-      }
-  
-      return instance;
-    }
-  }
-  
-  const car1: CarInterface = CarFactory.create('Amarok');
-  const car2: CarInterface = CarFactory.create('HB20');
-  const car3: CarInterface = CarFactory.create('Prisma');
-  const car4: CarInterface = CarFactory.create();
-  
-  [car1, car2, car3, car4].forEach(carro => {
-    console.log(`${carro.getName()} - ${carro.drive()}`)
-  });
+
+  const user: User = UserFactory.create('thalysonalexr', 23)
+
+  console.log(user)
 }
